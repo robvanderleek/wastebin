0a0fb19617fa6755616bc0691836de275910d734
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-10-03 18:00:45 +0200

    Show version number

--- bin.cpp
+++ bin.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+#include "config.hpp"
+
 using namespace std;
 
 int main(int argc, const char *argv[]) {
-    cout << "Hello world!" << endl;
+    auto message = "wastebin v";
+    cout << message << wastebin_VERSION_MAJOR << "." << 
+        wastebin_VERSION_MINOR << endl;
+    return 0;
 }
