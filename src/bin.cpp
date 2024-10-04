4818c7c169fad034a7ab1b6b660ea83a04a7e6f8
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-10-04 17:44:20 +0200

    Bin files and directories

--- src/bin.cpp
+++ src/bin.cpp
@@ -1,6 +1,22 @@
+#include <filesystem>
 #include "utils.hpp"
 
 int main(int argc, const char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+        return 1;
+    }
+    std::filesystem::path file(argv[1]);
+    std::cout << "Path: " << file << std::endl;
+    std::filesystem::path absPath = std::filesystem::absolute(file);
+    bool isFile = std::filesystem::is_regular_file(absPath);
+    std::cout << "Filename: " << absPath.filename() << std::endl;
+    std::cout << "Absolute path: " << absPath << std::endl;
+    std::filesystem::path wastebinRoot("/Users/rob/Desktop/wastebin");
+    std::filesystem::path targetPath;
+    targetPath = wastebinRoot / absPath.filename();
+    std::filesystem::rename(absPath, targetPath);
+    std::cout << "Moved to: " << targetPath << std::endl;
     wastebin::version();
     return 0;
 }
